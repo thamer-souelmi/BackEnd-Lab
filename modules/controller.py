e0b5f56bdfad40dac01c8ba95b926e3c5f2f42dd
thamer-souelmi <thamer.souelmi@esprit.tn>
2026-05-06 13:34:19 +0100

    Add JWT authentication (login + route protection)

--- modules/controller.py
+++ modules/controller.py
@@ -4,6 +4,8 @@ from http import HTTPStatus
 from modules.service import BookService
 from modules.livres import LivreModel
 from common.helpers.filters import Filters
+from auth.deps import verify_token
+from fastapi import Depends
 
 router = APIRouter()
 
@@ -12,13 +14,13 @@ service = BookService()
 LIVRE_URL = "/livres"
 
 
-@router.post(LIVRE_URL)
+@router.post(LIVRE_URL, dependencies=[Depends(verify_token)])
 def add(payload: LivreModel):
     inserted_model = service.add_model(payload)
     return inserted_model
 
 
-@router.put(f"{LIVRE_URL}/{{_id}}")
+@router.put(f"{LIVRE_URL}/{{_id}}", dependencies=[Depends(verify_token)])
 def update(_id: str, payload: LivreModel):
     updated = service.update_model(_id, payload)
 
@@ -31,14 +33,14 @@ def update(_id: str, payload: LivreModel):
     return updated
 
 
-@router.get(LIVRE_URL)
+@router.get(LIVRE_URL, dependencies=[Depends(verify_token)])
 def get_all(query_params: dict = {}):
     criteria = Filters(**query_params)
     models = service.get_all_models(filters=criteria)
     return models
 
 
-@router.get(f"{LIVRE_URL}/{{_id}}")
+@router.get(f"{LIVRE_URL}/{{_id}}", dependencies=[Depends(verify_token)])
 def get_by_id(_id: str):
     result = service.get_model(_id)
 
@@ -50,7 +52,7 @@ def get_by_id(_id: str):
 
     return result
 
-@router.delete(f"{LIVRE_URL}/{{_id}}", status_code=HTTPStatus.NO_CONTENT)
+@router.delete(f"{LIVRE_URL}/{{_id}}", status_code=HTTPStatus.NO_CONTENT, dependencies=[Depends(verify_token)])
 def delete(_id: str):
     success = service.delete_model(_id)
 
